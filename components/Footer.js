e916eee00c436cf873e32ed29e815dad02b8c54d
FishToad <61062479+FishToad@users.noreply.github.com>
2021-05-13 03:49:51 +0200

    Update and rename Header.js to Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+
+function Footer() {
+    return (
+        <div>
+            <div className='Footer'>
+            </div>
+            <style jsx>
+          {'
+            .Footer {
+              background-color: black
+            }
+          '}
+        </div>
+    )
+}
+
+export default Footer
