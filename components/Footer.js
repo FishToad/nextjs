0ef90ec20b86691f76916e6c3f71f08ea008c229
FishToad <61062479+FishToad@users.noreply.github.com>
2021-05-13 03:52:10 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,13 @@ function Footer() {
     return (
         <div>
             <div className='Footer'>
+                test123
             </div>
             <style jsx>
           {'
             .Footer {
-              background-color: black
+              background-color: black;
+              color: red
             }
           '}
         </div>
